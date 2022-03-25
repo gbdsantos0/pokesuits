42abf297c480e184e52d56d8bde52394898ae364
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-25 15:17:15 -0300

    transformado entity Mochila para JPA

--- src/main/java/com/dbc/pokesuits/entity/MochilaEntity.java
+++ src/main/java/com/dbc/pokesuits/entity/MochilaEntity.java
@@ -1,194 +1,49 @@
 package com.dbc.pokesuits.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Builder
-@Data
+@Entity(name = "MOCHILA")
 public class MochilaEntity {
-//    private ArrayList<Pokemon> bag;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MOCHILA_SEQ")
+    @SequenceGenerator(name = "MOCHILA_SEQ", sequenceName = "seq_mochila", allocationSize = 1)
+    @Column(name = "id_mochila")
     private int idMochila;
-    @NotNull
-    @Size(min=0, max=99)
+
+    @Column(name = "quantidadegreatballs")
     private int quantidadeGreatBalls;
-    @NotNull
-    @Size(min=0, max=99)
+
+    @Column(name = "quantidadeheavyballs")
     private int quantidadeHeavyBalls;
-    @NotNull
-    @Size(min=0, max=99)
+
+    @Column(name = "quantidademasterballs")
     private int quantidadeMasterBalls;
-    @NotNull
-    @Size(min=0, max=99)
+
+    @Column(name = "quantidadenetballs")
     private int quantidadeNetBalls;
-    @NotNull
-    @Size(min=0, max=99)
+
+    @Column(name = "quantidadepokeballs")
     private int quantidadePokeBalls;
 
-//    public Mochila() {
-//        this.bag = new ArrayList<>();
-//        this.quantidadeGreatBalls = 0;
-//        this.quantidadeHeavyBalls = 0;
-//        this.quantidadeMasterBalls = 0;
-//        this.quantidadeNetBalls = 0;
-//        this.quantidadePokeBalls = 0;
-//    }
+    @JsonIgnore
+    @OneToMany(mappedBy="mochila", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private Set<PokemonEntity> pokemons;
 
-
-    /*private Optional<Pokemon> pesquisarPorNome(String nomePokemon){
-        return this.bag.stream().filter( p -> p.getNome().equalsIgnoreCase(nomePokemon)).findFirst(); 
-    }
-    //CIRAR
-    public void adicionarPokemom(Pokemon p){
-        this.bag.add(p);
-    }
-
-    //CIRAR VARIOS
-    public void adicionarPokemons(List<Pokemon> p){
-        this.bag.addAll(p);
-    }
-
-    //ATUALIZAR
-    public void atualizarApelidoPokemon(Scanner scanner){
-        System.out.println("Qual pokemon deseja renomear:");
-        this.imprimir();
-        System.out.println();
-        Optional<Pokemon> pokemon = this.pesquisarPorNome(scanner.nextLine());
-
-        if(pokemon.isPresent()){
-            System.out.println("Digite um novo Nome: ");
-            Pokemon p = pokemon.get();
-            this.bag.remove(p);
-            p.setNome(scanner.nextLine());
-            this.bag.add(p);
-        }else{
-            System.out.println("Este pokemon não existe!! ");
-        }
-    }
-
-    //REMOVER
-    public Integer removerPokemom(Scanner scanner){
-        System.out.println("qual pokemon você deseja assassinar friamente: ");
-        this.imprimir();
-        System.out.println();
-        Optional<Pokemon> pokemon = this.pesquisarPorNome(scanner.nextLine());
-
-        if(pokemon.isPresent()){
-            this.bag.remove(pokemon.get());
-            System.out.println("Este pokemon \"retirado\"!! ");
-            return pokemon.get().getId();
-        }else{
-            System.out.println("Este pokemon não existe!! ");
-        }
-		return null;
-    }*/
-    //MOSTRAR
-    public void imprimir() {
-//        this.bag.forEach(p -> System.out.println("======================\n" + p + "\n============================\n"));
-    }
-
-    //getter
-    /*public List<Pokemon> getBag() {
-        //retorna uma lista não modificavel;
-        return Collections.unmodifiableList(this.bag);
-    }*/
-
-//    public int getIdMochila() {
-//        return idMochila;
-//    }
-//
-//    public int getQuantidadeGreatBalls() {
-//        return quantidadeGreatBalls;
-//    }
-//
-//    public int getQuantidadeHeavyBalls() {
-//        return quantidadeHeavyBalls;
-//    }
-//
-//    public int getQuantidadeMasterBalls() {
-//        return quantidadeMasterBalls;
-//    }
-//
-//    public int getQuantidadeNetBalls() {
-//        return quantidadeNetBalls;
-//    }
-//
-//    public int getQuantidadePokeBalls() {
-//        return quantidadePokeBalls;
-//    }
-//
-//    //setters
-//    public void setIdMochila(int idMochila) {
-//        this.idMochila = idMochila;
-//    }
-//
-//    public boolean setQuantidadeGreatBalls(int quantidadeGreatBalls) {
-//        if(quantidadeGreatBalls>=0 && quantidadeGreatBalls<=99){
-//            this.quantidadeGreatBalls = quantidadeGreatBalls;
-//            return true;
-//        }else{
-//            System.out.println("Não é possível modificar a quantidade para valores negativos ou maiores que 99");
-//            return false;
-//        }
-//    }
-//
-//    public boolean setQuantidadeHeavyBalls(int quantidadeHeavyBalls) {
-//        if(quantidadeHeavyBalls>=0 && quantidadeHeavyBalls<=99){
-//            this.quantidadeHeavyBalls = quantidadeHeavyBalls;
-//            return true;
-//        }else{
-//            System.out.println("Não é possível modificar a quantidade para valores negativos ou maiores que 99");
-//            return false;
-//        }
-//    }
-//
-//    public boolean setQuantidadeMasterBalls(int quantidadeMasterBalls) {
-//        if(quantidadeMasterBalls>=0 && quantidadeMasterBalls<=99){
-//            this.quantidadeMasterBalls = quantidadeMasterBalls;
-//            return true;
-//        }else{
-//            System.out.println("Não é possível modificar a quantidade para valores negativos ou maiores que 99");
-//            return false;
-//        }
-//    }
-//
-//    public boolean setQuantidadeNetBalls(int quantidadeNetBalls) {
-//        if(quantidadeNetBalls>=0 && quantidadeNetBalls<=99){
-//            this.quantidadeNetBalls = quantidadeNetBalls;
-//            return true;
-//        }else{
-//            System.out.println("Não é possível modificar a quantidade para valores negativos ou maiores que 99");
-//            return false;
-//        }
-//    }
-//
-//    public boolean setQuantidadePokeBalls(int quantidadePokeBalls) {
-//        if(quantidadePokeBalls>=0 && quantidadePokeBalls<=99){
-//            this.quantidadePokeBalls = quantidadePokeBalls;
-//            return true;
-//        }else{
-//            System.out.println("Não é possível modificar a quantidade para valores negativos ou maiores que 99");
-//            return false;
-//        }
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Mochila{" +
-//                "idMochila=" + idMochila +
-//                ", quantidadeGreatBalls=" + quantidadeGreatBalls +
-//                ", quantidadeHeavyBalls=" + quantidadeHeavyBalls +
-//                ", quantidadeMasterBalls=" + quantidadeMasterBalls +
-//                ", quantidadeNetBalls=" + quantidadeNetBalls +
-//                ", quantidadePokeBalls=" + quantidadePokeBalls +
-//                '}';
-//    }
+    @JsonIgnore
+    @OneToOne(mappedBy = "mochila")
+    private TreinadorEntity treinador;
 }
 
 
