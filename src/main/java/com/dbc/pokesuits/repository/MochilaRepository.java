1090cefca1046f9d4330af2ac3ee198258a0a8dd
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-25 22:52:03 -0300

    retirado imports inutilizados

--- src/main/java/com/dbc/pokesuits/repository/MochilaRepository.java
+++ src/main/java/com/dbc/pokesuits/repository/MochilaRepository.java
@@ -1,14 +1,9 @@
 package com.dbc.pokesuits.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dbc.pokesuits.entity.MochilaEntity;
-import com.dbc.pokesuits.exceptions.InvalidCenarioException;
 
 @Repository
 public interface MochilaRepository extends JpaRepository<MochilaEntity, Integer> {
